3dd1a6cfd53a01a519f15d114e16959f69ede595
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-07-07 18:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (7.86%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,39 +1,42 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int col0 = 1;
         int m = matrix.length;
         int n = matrix[0].length;
-        List<Integer> l= new ArrayList<>();
-        List<Integer> r= new ArrayList<>();
 
-        for(int i=0;i<m;i++){
-            for(int j =0;j<n;j++){
+        for(int i =0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                
                 if(matrix[i][j]==0){
-                    l.add(i);
-                    r.add(j);
-                    
+                    matrix[i][0] =0;
 
+                    if(j!=0)matrix[0][j] = 0;
+
+
+                    else col0 = 0;
                 }
             }
         }
-        System.out.print(l);
-        System.out.print(r);
-        
-        for(int i= 0;i<l.size();i++){
-            fun(matrix,l.get(i),r.get(i));
+        for(int i =1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
         }
-                    
+
+
+        if(matrix[0][0]==0){
+            for(int i =0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
+            for(int i =0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
         
-    }
-    static void fun(int a[][],int p,int q){
-       int m = a.length;
-       int n = a[0].length;
 
-       for(int i =0;i<m;i++){
-        a[i][q] = 0;
-       }
-       for(int i =0;i<n;i++){
-        a[p][i] = 0;
-
-       }
     }
 }
