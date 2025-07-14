168579a653fec1e335e7c63dd8408471157fc112
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-07-14 18:26:42 +0530

    Time: 1 ms (83.87%) | Memory: 41.7 MB (87.57%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,29 +1,32 @@
 class Solution {
     public int numDecodings(String s) {
-        Integer dp[]= new Integer[s.length()+1];
-        return fun(s,0,dp);
+        Integer dp[] =new Integer[s.length()+1];
+
+        
+       
+
+        return fun(s,0,s.length(),dp);
         
     }
-    static int fun(String s,int i,Integer dp[]){
-        if(i>=s.length()) return 1;// suceccesfully reaching the end  
+    static int fun(String s,int i,int n,Integer dp[]){
+        if(i>=n ) return 1;
         if(s.charAt(i)=='0') return 0;
         if(dp[i]!=null) return dp[i];
 
-        int a = fun(s,i+1,dp);
 
+        
 
-        int b = 0;
-        if(i+1<s.length()){
-            int val = Integer.parseInt(s.substring(i,i+2));
+        int c1 = fun(s,i+1,n,dp);
+        int c2 = 0;
 
-            if(val>=10 && val<=26){
-                b = fun(s,i+2,dp);
-            }
+        if(i+1<n){
+            int num = Integer.parseInt(s.substring(i,i+2));
+
+            if(num>=10 && num<=26) c2 = fun(s,i+2,n,dp);
         }
-        return dp[i]=a+b;
 
-        
 
-        
+        return dp[i] = c1+c2;
+
     }
 }
