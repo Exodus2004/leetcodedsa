ff63eff614d4e666d6a0737f8d4f77add12a2e5f
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-07-14 18:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (22.72%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,31 +1,26 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         Integer dp[] = new Integer[days.length];
-        return fun(days,costs,0,days.length,dp);
+        return fun(days,costs,0,dp);
         
     }
-    static int fun(int a[],int b[],int i,int n,Integer dp[]){
-
-        if(i>=n) return 0;
+    static int fun(int a[],int c[],int i,Integer dp[]){
+        if(i>=a.length) return 0;
         if(dp[i]!=null) return dp[i];
 
+        int c1 = c[0]+fun(a,c,i+1,dp);
 
+        int next7 = i+1;
+        while(next7<a.length && a[next7]<=a[i]+6) next7++;
 
-        int c1=b[0]+fun(a,b,i+1,n,dp);
+        int c2 = c[1] +fun(a,c,next7,dp);
+        int next30 =i+1;
+        while(next30<a.length && a[next30]<=a[i]+29) next30++;
 
-       int next7 =0;
-       while(next7<n && a[next7]<=a[i]+6){
-        next7++;
-       }
-       int c2 = b[1]+fun(a,b,next7,n,dp);
+        int c3 = c[2]+fun(a,c,next30,dp);
 
-       int next30 = 0;
-       while(next30<n && a[next30]<=a[i]+29){
-        next30++;
-       }
-       int c3 = b[2]+fun(a,b,next30,n,dp);
-
-
-        return dp[i]=Math.min(c1,Math.min(c3,c2));
+        return dp[i] = Math.min(c1,Math.min(c2,c3));
+            
+        
     }
 }
