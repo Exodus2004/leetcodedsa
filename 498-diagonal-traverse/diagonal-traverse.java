59942966c7da1333dbf07df9146b280b8937ea0f
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-07-20 11:10:00 +0530

    Time: 9 ms (18.72%) | Memory: 48.1 MB (8.70%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -1,45 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
         List<List<Integer>> list = new ArrayList<>();
 
-        for(int i =0;i<m+n;i++) list.add(new ArrayList<>());
+        // Initialize list of lists to hold diagonals
+        for (int i = 0; i < m + n; i++) {
+            list.add(new ArrayList<>());
+        }
 
-        
-
-       
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(i==0 && j==0) continue;
-                
-                    list.get(i+j).add(mat[i][j]);
-                
-                
+        // Group elements by their i + j value (diagonal)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                list.get(i + j).add(mat[i][j]);
             }
-            
         }
+
+        // Build the result list, reversing even-numbered diagonals
         List<Integer> res = new ArrayList<>();
-        res.add(mat[0][0]);
-        for(int i =0;i<m+n;i++){
-            if(i%2==0) Collections.reverse(list.get(i));
-
-            for(int j :list.get(i)) res.add(j);
+        for (int i = 0; i < list.size(); i++) {
+            if (i % 2 == 0) {
+                Collections.reverse(list.get(i));
+            }
+            res.addAll(list.get(i));
         }
-        System.out.println(res);
-        
 
-        int a[] = new int[res.size()];
-        for(int i=0;i<res.size();i++) a[i] = res.get(i);
-
-        return a;
-
-
-
-
-
-
-        
+        // Convert to array and return
+        int[] ans = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            ans[i] = res.get(i);
+        }
+        return ans;
     }
-    // 00 01 10 20 11 02 12 21 22 
 }
