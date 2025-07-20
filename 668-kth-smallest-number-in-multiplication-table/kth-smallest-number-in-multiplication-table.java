22c2ac4909e4d3eae0bec309dc3b6e40e6061508
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-07-20 18:51:34 +0530

    Time: 13 ms (7.62%) | Memory: 40.4 MB (54.73%) - LeetSync

---
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int l = 1;
+        int r = m*n;
+
+        while(l<r){
+            int mid = (l+r)/2;
+            int count = 0;
+
+
+            for(int i=1;i<=m;i++){
+                count+=Math.min(mid/i,n);
+
+            }
+
+            
+
+            if(count<k) l = mid+1;
+            else r =mid;
+        }
+        System.out.println(l);
+        return l;
+
+        
+    }
+}
