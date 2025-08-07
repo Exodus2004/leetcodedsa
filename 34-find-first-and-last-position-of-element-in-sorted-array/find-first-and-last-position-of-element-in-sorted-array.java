171e69db693543674c79797c2b37ae4813fa6572
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-08-07 17:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (16.52%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int a[] = {-1,-1};
-        a[0] = fun(nums,0,nums.length-1,target);
-        a[1] = fun2(nums,0,nums.length-1,target);
-      
-      return a;
-    }
-    static int fun(int a[],int i,int j,int k){
-        int idx = -1;
-        while(i<=j){
-            int m = (i+j)/2;
-            if(a[m]==k){
-                idx = m;
-                j = m-1;
-            }
+        
+        int a[] = new int[2];
+        if(nums.length==0) return new int[]{-1,-1};
 
-            else if(a[m]<k) i = m+1; 
-            else j = m-1;
-        }
-        return idx;
-    }
-    static int fun2(int a[],int i,int j,int k){
-        int idx = -1;
-        while(i<=j){
-            int m = (i+j)/2;
-            if(a[m]==k){
-                idx = m;
-                i = m+1;
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            int m =(l+r)/2;
+            if(nums[m]==target){
+                r = m-1;
             }
-
-            else if(a[m]<k) i = m+1; 
-            else j = m-1;
+            else if(nums[m]>target) r = m-1;
+            else l = m+1;
         }
-        return idx;
+        if(l<0 || l>=nums.length || nums[l]!=target) a[0] = -1;
+        else a[0] = l;
+
+
+        l = 0;
+         r = nums.length-1;
+        while(l<=r){
+            int m =(l+r)/2;
+            if(nums[m]==target){
+                l = m+1;
+            }
+            else if(nums[m]>target) r = m-1;
+            else l = m+1;
+        }
+        if(r<0 || r>=nums.length || nums[r]!=target) a[1] = -1;
+        else a[1] = r;
+
+        return a;
+
+        
     }
-    
+
 }
