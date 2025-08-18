e301431d4697250601a78468a80727294e1021bd
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-08-18 15:41:17 +0530

    Time: 2 ms (87.17%) | Memory: 56 MB (26.33%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int a[] = new int[n];
-        int c = 1;
-        Arrays.fill(a,1);
+        int pre[] = new int[n];
+        int suf[] = new int[n];
 
-       for(int i =0;i<n;i++){
-        a[i] *= c;
-        c *= nums[i];
-       }
-        c=1;
-       for(int i =n-1;i>=0;i--){
-        a[i] *= c;
-        c *= nums[i];
-       }
+        pre[0] = nums[0];
+        suf[n-1] = nums[n-1];
+
+        for(int i =1;i<n;i++){
+            pre[i] = pre[i-1]*nums[i];
+        }
+        for(int i = n-2;i>=0;i--) suf[i] = suf[i+1] * nums[i];
+        int a[] = new int[n];
+        for(int i =1;i<n-1;i++){
+            a[i] = pre[i-1] * suf[i+1];
+        }
+        a[0] = suf[1];
+        a[n-1] = pre[n-2];
+
+        
         return a;
+
         
     }
 }
