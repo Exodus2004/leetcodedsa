06770f22d1558450c088479196909720f35343c2
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-08-18 15:10:20 +0530

    Time: 3 ms (82.59%) | Memory: 45 MB (74.46%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,32 +1,26 @@
 class Solution {
     public String reverseVowels(String s) {
-        StringBuilder sb = new StringBuilder();
-        StringBuilder res = new StringBuilder();
-        for(int i =0;i<s.length();i++){
-            if(fun(s.charAt(i))) sb.append(s.charAt(i));
+        int left =0;
+        int right = s.length()-1;
+        char a[] = s.toCharArray();
+
+        while(left<right){
+
+            while(left<right && fun(a[left])) left++;
+            while(left<right && fun(a[right])) right--;
+
+            char t = a[right];
+            a[right] = a[left];
+            a[left] = t;
+            left++;
+            right--;
 
         }
-        sb.reverse();
-        int k =0;
-        for(int i =0;i<s.length();i++){
-            if(fun(s.charAt(i))){
-                res.append(sb.charAt(k));
-                k++;
+        return new String(a);
 
-            }
-            else{
-                res.append(s.charAt(i));
-            }
-
-        }
-        return res.toString();
         
     }
     boolean fun(char c){
-        
-        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') return true;
-        if(c=='A'||c=='E'||c=='I'||c=='O'||c=='U') return true;
-        
-        return false;
+        return "aeiouAEIOU".indexOf(c)==-1;
     }
 }
