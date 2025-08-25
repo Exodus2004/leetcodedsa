e391d67d5e40a4a9b032d2d83d47db62c5046355
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-08-25 20:28:38 +0530

    Time: 2 ms (87.05%) | Memory: 56.1 MB (23.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int pre[] = new int[n];
-        int suf[] = new int[n];
-
+        int a[] = new int[n];
+        int pre [] = new int[n];
+        int suf [] = new int[n];
         pre[0] = nums[0];
-        suf[n-1] = nums[n-1];
-
-        for(int i =1;i<n;i++){
+        suf[n-1]= nums[n-1];
+        for(int i = 1;i<n;i++){
             pre[i] = pre[i-1]*nums[i];
         }
-        for(int i = n-2;i>=0;i--) suf[i] = suf[i+1] * nums[i];
-        int a[] = new int[n];
+        for(int i = n-2;i>=0;i--){
+            suf[i] = suf[i+1]*nums[i];
+
+        }
         for(int i =1;i<n-1;i++){
-            a[i] = pre[i-1] * suf[i+1];
+            a[i] = suf[i+1]*pre[i-1];
+
         }
         a[0] = suf[1];
         a[n-1] = pre[n-2];
-
-        
         return a;
 
         
