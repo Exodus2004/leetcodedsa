cbfb205ed8e3cada4f35c7aa0f0cfa0b40a77729
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-08-27 00:00:10 +0530

    Time: 25 ms (35.80%) | Memory: 44.5 MB (31.11%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,52 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length()<s1.length()) return false;
-        int a[] = new int[26];
-    int b[] = new int[26];
+        if (s1.length() > s2.length()) return false;
 
-    for(int i =0;i<s1.length();i++){
-        a[s1.charAt(i)-'a']++;
-        b[s2.charAt(i)-'a']++;
-        
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
 
+        // Build frequency map of s1
+        for (char c : s1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int l = 0;
+        int r = 0;
+
+        while (r < s2.length()) {
+            // Add s2[r] to window
+            char rightChar = s2.charAt(r);
+            window.put(rightChar, window.getOrDefault(rightChar, 0) + 1);
+
+            // Check if current window has the target length
+            if (r - l + 1 == s1.length()) {
+                // Check if it's an anagram of s1
+                if (fun(map, window)) {
+                    return true;
+                }
+
+                // Remove leftmost character from window
+                char leftChar = s2.charAt(l);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (window.get(leftChar) == 0) {
+                    window.remove(leftChar);
+                }
+                l++; // shrink window
+            }
+
+            r++;
+        }
+
+        return false;
     }
 
-    for(int i =0;i<s2.length()-s1.length();i++){
-        if(fun(a,b)) return true;
-
-        b[s2.charAt(i)-'a']--;
-        b[s2.charAt(i+s1.length())-'a']++;
-    }
-    return (fun(a,b));
-        
-    }
-    boolean fun(int a[],int b[]){
-        for(int i =0;i<26;i++){
-            if(a[i]!=b[i]) return false;
+    // Check if window has same frequency as s1
+    boolean fun(Map<Character, Integer> map, Map<Character, Integer> window) {
+        if (map.size() != window.size()) return false;
+        for (char c : map.keySet()) {
+            if (!window.containsKey(c) || !map.get(c).equals(window.get(c))) {
+                return false;
+            }
         }
         return true;
     }
