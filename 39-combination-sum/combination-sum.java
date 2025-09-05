7830d1e2dd9476409111a6d905516b41a368ff5f
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-05 10:15:33 +0530

    Time: 2 ms (83.77%) | Memory: 44.7 MB (45.94%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,24 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] a, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> temp = new ArrayList<>();
         List<List<Integer>> res = new ArrayList<>();
-        fun(0,0,a,target,temp,res);
+        fun(0,0,target,candidates,temp,res);
         return res;
-
-        
     }
-    void fun(int i,int sum,int a[],int target,List<Integer> temp,List<List<Integer>> res){
-        if(sum>target) return;
+    void fun(int i,int sum,int target,int a[],List<Integer> temp,List<List<Integer>> res){
+        if(sum> target) return;
         if(i>=a.length){
             if(sum==target){
                 res.add(new ArrayList<>(temp));
+                
             }
             return;
         }
         sum+=a[i];
         temp.add(a[i]);
-        fun(i,sum,a,target,temp,res);
+        fun(i,sum,target,a,temp,res);
         sum-=a[i];
         temp.remove(temp.size()-1);
-        fun(i+1,sum,a,target,temp,res);
-
+        fun(i+1,sum,target,a,temp,res);
     }
 }
