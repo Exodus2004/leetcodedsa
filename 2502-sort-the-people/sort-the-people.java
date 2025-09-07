811c263d41efdbc13c9723ac977d6d3ced91e0b2
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-07 15:06:42 +0530

    Time: 18 ms (27.71%) | Memory: 45.4 MB (80.39%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,22 +1,11 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        PriorityQueue<List<String>> pq = new PriorityQueue<>((a,b)->{
-            return Integer.parseInt(b.get(1))-Integer.parseInt(a.get(1));
-
-        });
-
-        for(int i =0;i<names.length;i++){
-            List<String> k =new ArrayList<>();
-            k.add(names[i]);
-            k.add(String.valueOf(heights[i]));
-
-            pq.add(k);
-        }
-        String a[] = new String[names.length];
-        for(int i=0;i<names.length;i++){
-            a[i] = pq.poll().get(0);
-        }
-        return a;
+        TreeMap<Integer,String> map = new TreeMap<>();
+        for(int i =0;i<names.length;i++) map.put(heights[i],names[i]);
+        System.out.println(map);
+        int i =  names.length-1;
+        for(String s:map.values()) names[i--] = s;
         
+        return names;
     }
 }
