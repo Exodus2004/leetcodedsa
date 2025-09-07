bcad7b5e0a36e5000b2800b85fa15df56d655c6e
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-07 09:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (86.14%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[] sumZero(int n) {
-        int a[] = new int[n];
-        TreeSet<Integer> res = new TreeSet<>();
-
-        if(n%2==0){
-            for(int i= 1;i<=n/2;i++){
-                res.add(i);
-                res.add(-1*i);
-
-            }
-
+        int a[ ] = new int[n];
+        int l =0;
+        int r = n-1;
+        int val = n/2;
+        while(l<r){
+            a[l] = -1*val;
+            a[r] = val;
+            l++;
+            r--;
+            val--;
+            
         }
-        else{
-            res.add(0);
-            for(int i=0;i<=n/2;i++){
-                
-                res.add(i);
-                res.add(-1*i);
-            }
-
-        }
-        System.out.println(res);
-        List<Integer> k = new ArrayList<>(res);
-        for(int i=0;i<n;i++) a[i] = k.get(i);
         return a;
         
     }
