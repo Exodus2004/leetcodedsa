f28124e4ddeeb5b361f1c4a70ad9023fe6ec1c74
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-07 10:27:09 +0530

    Time: 8 ms (50.53%) | Memory: 45.5 MB (39.26%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,30 +1,17 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        
-
+        Map<Integer,List<Integer>> map = new HashMap<>();
         List<List<Integer>> res = new ArrayList<>();
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
-            return a[1]-b[1];
-
-        });
-        for(int i=0;i<groupSizes.length;i++) pq.add(new int[]{i,groupSizes[i]});
-        List<Integer> temp = new ArrayList<>();
-        
-
-        while(!pq.isEmpty()){
-            int c[] = pq.poll();
-            temp.add(c[0]);
-            if(temp.size()==c[1]){
-            res.add(new ArrayList<>(temp));
-            temp.clear();
+        for(int i=0;i<groupSizes.length;i++){
+            int val = groupSizes[i];
+            map.putIfAbsent(val,new ArrayList<>());
+            map.get(val).add(i);
+            if(map.get(val).size()==val){
+                res.add(new ArrayList<>(map.get(val)));
+                map.get(val).clear();
             }
-
-
         }
         return res;
-
-
         
     }
 }
