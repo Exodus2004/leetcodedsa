51428ca22000d812276034fa19d1e0caa5475fc4
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-07 23:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (87.99%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,18 +1,9 @@
 class Solution {
     public boolean checkString(String s) {
-        int start = 0;
-        Set<Character> set = new HashSet<>();
-        for(char c:s.toCharArray()) set.add(c);
-        if(set.size()==1) return true;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='b'){
-                start = i;
-                break;
-            }
-        }
-        
-        for(int i=start;i<s.length();i++){
-            if(s.charAt(i)=='a') return false;
+        int n = s.length();
+        for(int i = 0;i<n-1;i++){
+            if(s.charAt(i)=='b' && s.charAt(i+1)=='a') return false;
+            
         }
         return true;
         
