5eff82be43297771107483aa9d40b66b078c36c4
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-10 16:41:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (94.75%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,42 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int col0 = 1;
-        int m = matrix.length;
-        int n = matrix[0].length;
+        boolean r = false;
+        boolean c = false;
+        int n = matrix.length;
+        int m = matrix[0].length;
 
-        for(int i =0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0] =0;
+                    if(i==0) r = true;
+                    if(j==0) c = true;
+                    matrix[0][j]= 0;
+                    matrix[i][0] = 0;
 
-                    if(j!=0)matrix[0][j] = 0;
-
-
-                    else col0 = 0;
                 }
             }
         }
-        for(int i =1;i<m;i++){
-            for(int j = 1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j] = 0;
-                }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[0][j]==0||matrix[i][0]==0) matrix[i][j] = 0;
             }
+        }
+        if(r){
+            for(int i =0;i<m;i++) matrix[0][i] = 0;
+
+        }
+        if(c){
+            for(int i=0;i<n;i++) matrix[i][0] = 0;
+
         }
 
-
-        if(matrix[0][0]==0){
-            for(int i =0;i<n;i++){
-                matrix[0][i]=0;
-            }
-        }
-        if(col0==0){
-            for(int i =0;i<m;i++){
-                matrix[i][0]=0;
-            }
-        }
         
-
     }
 }
