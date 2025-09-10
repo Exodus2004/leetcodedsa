6dfe96358bfa755734f125797d1dd9ae7c335ed3
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-10 19:06:47 +0530

    Time: 2 ms (87.20%) | Memory: 56.6 MB (5.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        int lp[] = new int[n];
+        int rp[] = new int[n];
+        lp[0] = nums[0];
+        for(int i =1;i<n;i++) lp[i] = lp[i-1]*nums[i];
+        rp[n-1] = nums[n-1];
+        for(int i =n-2;i>=0;i--) rp[i] = rp[i+1]*nums[i];
         int a[] = new int[n];
-        int pre [] = new int[n];
-        int suf [] = new int[n];
-        pre[0] = nums[0];
-        suf[n-1]= nums[n-1];
-        for(int i = 1;i<n;i++){
-            pre[i] = pre[i-1]*nums[i];
-        }
-        for(int i = n-2;i>=0;i--){
-            suf[i] = suf[i+1]*nums[i];
+
+        for(int i=1;i<n-1;i++){
+            a[i] = lp[i-1]*rp[i+1];
+
 
         }
-        for(int i =1;i<n-1;i++){
-            a[i] = suf[i+1]*pre[i-1];
-
-        }
-        a[0] = suf[1];
-        a[n-1] = pre[n-2];
+        a[0] =rp[1];
+        a[n-1] = lp[n-2];
         return a;
-
         
     }
 }
