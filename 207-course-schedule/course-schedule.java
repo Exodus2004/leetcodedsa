91341de151359bda395b306f8efdd2a43cd7d0bf
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-14 14:19:20 +0530

    Time: 6 ms (72.18%) | Memory: 46.1 MB (15.40%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int indeg[] = new int[numCourses];
+        int n = numCourses;
         List<List<Integer>> adj = new ArrayList<>();
-
-        for(int i =0;i<numCourses;i++) adj.add(new ArrayList<>());
-
-        for(int i[]:prerequisites ){
-
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int i[] : prerequisites) {
             adj.get(i[1]).add(i[0]);
-            indeg[i[0]]++;
-
+            a[i[0]]++;
         }
-        
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0;i<numCourses;i++){
-            if(indeg[i]==0) q.add(i);
-            
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0)
+                q.add(i);
         }
-        int count =0;
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-
+        List<Integer> res = new ArrayList<>();
+        int count = 0;
+        while (!q.isEmpty()) {
+            int val = q.poll();
             count++;
-            for(int i :adj.get(node)){
-                indeg[i]--;
-                if(indeg[i]==0){
+            for (int i : adj.get(val)) {
+                a[i]--;
+                if (a[i] == 0)
                     q.add(i);
-                }
             }
-
         }
-        return count==numCourses;
-        
+        return count == n;
+
     }
 }
