99e8c5b14db8889ab115ec63597190fbf18243f2
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-16 11:34:43 +0530

    Time: 16 ms (23.37%) | Memory: 47.8 MB (30.80%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,58 +1,43 @@
 class Solution {
-    class pair{
-        int x;
-        int y;
-        pair(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int a[][] =new int[n][m];
-        Queue<pair> q= new LinkedList<>();
-        int count = 1;
-
-        boolean vis[][] =new boolean[n][m];
-
-        for(int i=0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(mat[i][j]==0 && !vis[i][j]){
+        int a[][] = new int[n][m];
+        int count = 0;
+        Queue<int[]> q = new LinkedList<>();
+         boolean vis[][] = new boolean[n][m];
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0 && !vis[i][j]) {
                     vis[i][j] = true;
-                    q.add(new pair(i,j));
+                    q.add(new int[]{i,j});
                 }
 
             }
         }
+        int nr[] = {1,-1,0,0};
+        int nc[] = {0,0,-1,1};
 
         while(!q.isEmpty()){
-            int size = q.size();
-            for(int c =0;c<size;c++){
-                pair node = q.poll();
-                int x = node.x;
-                int y = node.y;
-                int dr[] = {0,0,-1,1};
-                int dc[] = {1,-1,0,0};
-
-                for(int i=0;i<4;i++){
-                    int nx = x+dr[i];
-                    int ny = y+dc[i];
-                    if(nx<n && ny<m && nx>=0 && ny>=0 && mat[nx][ny]==1 & !vis[nx][ny]){
+            int p = q.size();
+            for(int i=0;i<p;i++){
+                int val[] =q.poll();
+                int x = val[0];
+                int y = val[1];
+                a[x][y]= count;
+                for(int k =0;k<4;k++){
+                    int nx = x+nr[k];
+                    int ny = y+nc[k];
+                    if(nx>=0 && ny>=0 && nx<n && ny<m && mat[nx][ny]==1 && !vis[nx][ny]){
                         vis[nx][ny] = true;
-                        a[nx][ny] = count;
-                        q.add(new pair(nx,ny));
+                        q.add(new int[]{nx,ny});
                     }
                 }
-;                
                 
             }
-
             count++;
         }
         return a;
-        
 
-        
     }
 }
