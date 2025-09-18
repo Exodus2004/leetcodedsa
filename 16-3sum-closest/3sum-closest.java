010708887126511e209c8baaedd618480f3cc43b
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-18 10:52:51 +0530

    Time: 13 ms (90.55%) | Memory: 43.1 MB (77.67%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -2,21 +2,26 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
     Arrays.sort(nums);
     int n = nums.length;
-    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-     return Math.abs(a-target)-Math.abs(b-target);
-    });
+   
+    int res =0;
+    int diff = Integer.MAX_VALUE;
 
     for(int i =0;i<n-2;i++){
         int l = i+1;
         int r = n-1;
         while(l<r){
             int sum = nums[l]+nums[i]+nums[r];
-            pq.add(sum);
+            if(Math.abs(sum-target)<diff){
+                diff = Math.abs(sum-target);
+                res = sum;
+
+
+            }
             if(sum>target) r--;
             else l++;
         }
     }
-    return pq.poll();
+    return res;
         
     }
 }
