6c4acfd0948afddc19d1e8fdb1722edf3f40fc4a
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2025-09-21 17:08:03 +0530

    Time: 24 ms (14.66%) | Memory: 45 MB (16.16%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -6,7 +6,7 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<n-3;i++){
             for(int j = i+1;j<n-2;j++){
-                
+                if(i!=0 && nums[i]==nums[i-1]) continue;
                 int l = j+1;
                 int r = n-1;
                 while(l<r){
@@ -15,6 +15,7 @@ class Solution {
                         res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
                         l++;
                         r--;
+                        
                     }
                     else if(sum>target) r--;
                     else l++;
