07f35f036222b79e68a6233c1fb54f01f1f22a79
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-02-21 16:39:59 +0530

    Time: 5 ms (64.72%) | Memory: 42.4 MB (44.84%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -10,7 +10,9 @@ class Solution {
     }
     boolean prime(int n){
         if(n<=1) return false;
-        for(int i = 2;i<n;i++){
+        if(n==2) return true;
+         if (n % 2 == 0) return false;
+        for(int i = 3;i*i<=n;i=i+2){
             if(n%i==0) return false;
         }
         return true;
