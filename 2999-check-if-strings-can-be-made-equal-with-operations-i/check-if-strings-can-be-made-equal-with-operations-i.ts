c5b5ca836988f5a9776f1fb001e51d403cc5ade5
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-03-29 07:46:31 +0530

    Time: 1 ms (77.78%) | Memory: 58 MB (55.56%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.ts
@@ -0,0 +1,11 @@
+function canBeEqual(s1: string, s2: string): boolean {
+  if(s1 === s2)
+    return true;
+  if(s1 === (s2[2]+s2[1]+s2[0]+s2[3]))
+    return true;
+  if(s1 === (s2[0]+s2[3]+s2[2]+s2[1]))
+    return true;
+  if(s1 === (s2[2]+s2[3]+s2[0]+s2[1]))
+    return true;
+  return false;
+};
