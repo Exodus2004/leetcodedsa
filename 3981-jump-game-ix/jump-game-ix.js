41a5bc554aaf0bb485642738011bdd3040f0683a
Marthala Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-05-07 05:37:11 +0530

    Time: 65 ms (73.53%) | Memory: 95.3 MB (91.18%) - LeetSync

---
+++ 3981-jump-game-ix/jump-game-ix.js
@@ -0,0 +1,29 @@
+function maxValue(a) {
+    const n = a.length;
+    const pre = new Array(n);
+    const suff = new Array(n);
+    const res = new Array(n);
+
+    pre[0] = a[0];
+    suff[n - 1] = a[n - 1];
+
+    for (let i = 1; i < n; i++) {
+        pre[i] = Math.max(a[i], pre[i - 1]);
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        suff[i] = Math.min(a[i], suff[i + 1]);
+    }
+
+    res[n - 1] = pre[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        res[i] = pre[i];
+
+        if (pre[i] > suff[i + 1]) {
+            res[i] = res[i + 1];
+        }
+    }
+
+    return res;
+}
