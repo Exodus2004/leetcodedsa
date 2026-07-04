124d717d1221577d6f5741d1c9e313c1e0178190
Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-07-04 09:04:18 +0530

    Time: 66 ms (88.00%) | Memory: 94.4 MB (84.00%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.js
@@ -0,0 +1,16 @@
+const minScore = (n, roads) => {
+    const root = [...Array(n + 1).keys()];
+    const find = i => root[i] === i ? i : root[i] = find(root[i]);
+
+    for (let i = 0; i < roads.length; i++)
+        root[find(roads[i][0])] = find(roads[i][1]);
+
+    roads = roads.filter(r => find(r[0]) === find(1));
+
+    let min = Infinity;
+
+    for (const [, , d] of roads)
+        min = Math.min(min, d);
+
+    return min;
+};
