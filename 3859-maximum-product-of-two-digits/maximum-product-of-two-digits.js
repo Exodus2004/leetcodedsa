26dffaa8597603b4474082f575c1239d661d1dff
Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-07-25 09:22:08 +0530

    Time: 1 ms (80.85%) | Memory: 56 MB (63.83%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.js
@@ -0,0 +1,14 @@
+const maxProduct = n => {
+    let max1 = 0, max2 = 0;
+
+    for (; n; n = Math.floor(n / 10)) {
+        let d = n % 10;
+        if (d >= max1) {
+            max2 = max1;
+            max1 = d;
+        } else if (d > max2) 
+            max2 = d;
+    }
+
+    return max1 * max2;
+};
