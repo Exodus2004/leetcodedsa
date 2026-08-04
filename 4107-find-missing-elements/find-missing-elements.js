11a234f916ec945c4d610e8d41d4956c8ce5fc77
Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-08-04 08:05:57 +0530

    Time: 4 ms (40.94%) | Memory: 58.5 MB (50.39%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.js
@@ -0,0 +1,17 @@
+var findMissingElements = function(nums) {
+
+    let mn = Math.min(...nums);
+    let mx = Math.max(...nums);
+
+    const seen = new Set(nums);
+
+    const ans = [];
+
+    for (let x = mn + 1; x < mx; x++) {
+
+        if (!seen.has(x))
+            ans.push(x);
+    }
+
+    return ans;
+};
