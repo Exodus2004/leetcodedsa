6477012f7ea06589967daf446c39f4291af80de1
Kiran Reddy <160846178+Exodus2004@users.noreply.github.com>
2026-09-20 13:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (67.06%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.js
@@ -0,0 +1,10 @@
+var reverseDegree = function(s) {
+    let sum = 0;
+    for (let i = 0; i < s.length; i++) {
+        const reverseValue = 26 - (s.charCodeAt(i) - 97);
+        const position = i + 1;
+
+        sum += reverseValue * position;
+    }
+    return sum;
+};
